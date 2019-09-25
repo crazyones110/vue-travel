778c897fcdb8b058016cf15406e69ac5f74dc9ce
craynones <crazyones110@gmail.com>
2019-09-25 16:30:18 +0800

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide>
+                <img class="swiper-img" src="@/assets/imgs/swiper-1.jpg">
+            </swiper-slide>
+            <swiper-slide>
+                <img class="swiper-img" src="@/assets/imgs/swiper-2.jpg">
+            </swiper-slide>
+            <swiper-slide>
+                <img class="swiper-img" src="@/assets/imgs/swiper-3.jpg">
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomeSwiper",
+    data () {
+        return {
+            swiperOption: {
+                loop: true,
+                pagination: {
+                    el: ".swiper-pagination"
+                }
+            },
+            imgSrc: [
+                ""
+            ]
+        }
+    }
+}
+</script>
+<style lang="sass">
+.swiper-pagination-bullet-active
+    background-color: #fff
+</style>>
+    
+</style>
+<style lang="sass" scoped>
+// .wrapper >>> .swiper-pagination-bullet-active
+//     background-color: red
+// 这里穿透为啥不好使？
+.wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 66.7%
+    // 还可以写 height: 31.25vw
+    background-color: #eee
+.swiper-img
+    width: 100%
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <home-header></home-header>
+        <home-swiper></home-swiper>
+        <div>test</div>
     </div>
 </template>
 
 <script>
 import homeHeader from "./components/Header"
+import homeSwiper from "./components/Swiper"
 export default {
   name: "Home",
   components: {
-      homeHeader
+      homeHeader,
+      homeSwiper
   }
 }
 </script>
