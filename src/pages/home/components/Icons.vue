607b11c50c55e8da628f7e0ea00617f45e92002b
craynones <crazyones110@gmail.com>
2019-09-25 19:50:22 +0800

    icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+            <div class="icon-wrapper">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                <p>热门景点</p>
+            </div>            
+        </div>
+        <div class="icon">
+            <div class="icon-wrapper">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                <p>热门景点</p>
+            </div>            
+        </div>
+        <div class="icon">
+            <div class="icon-wrapper">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                <p>热门景点</p>
+            </div>            
+        </div>
+        <div class="icon">
+            <div class="icon-wrapper">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                <p>热门景点</p>
+            </div>            
+        </div>
+        <div class="icon">
+            <div class="icon-wrapper">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                <p>热门景点</p>
+            </div>            
+        </div>
+        <div class="icon">
+            <div class="icon-wrapper">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                <p>热门景点</p>
+            </div>            
+        </div>
+        <div class="icon">
+            <div class="icon-wrapper">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                <p>热门景点</p>
+            </div>            
+        </div>
+        <div class="icon">
+            <div class="icon-wrapper">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+                <p>热门景点</p>
+            </div>            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "homeIcons"
+}
+</script>
+
+<style lang="sass" scoped>
+.icons
+    box-sizing: border-box
+    height: 0
+    overflow: hidden
+    padding-bottom: 50%
+    .icon
+        width: 25%
+        float: left
+        padding-bottom: 25%
+        position: relative
+        .icon-wrapper
+            width: 100%
+            height: 100%
+            position: absolute
+            top: 0
+            left: 0
+            display: flex
+            flex-direction: column
+            align-items: center
+            justify-content: center
+        img
+            width: 70%
+</style>
