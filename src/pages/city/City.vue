5182d6420817cd4b392a3d7f125f65761b4e3824
craynones <crazyones110@gmail.com>
2019-09-29 10:28:24 +0800

    finish city-search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,18 +1,24 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-search></city-search>
     </div>
 </template>
 
 <script>
 import cityHeader from "./components/Header"
+import citySearch from "./components/Search"
+
 export default {
     name: "city",
     components: {
-        cityHeader
+        cityHeader,
+        citySearch
     }
 }
 </script>
 
 <style lang="sass" scoped>
+@import ~styles/variables.sass
+
 </style>
