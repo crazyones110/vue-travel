0e80e1cb7ed78ceed593860452cd8915de5c4008
crazyones110 <crazyones110@gmail.com>
2019-10-07 00:40:32 +0800

    fixed css

--- src/common/gallery/Gallery.vue
+++ src/common/gallery/Gallery.vue
@@ -60,7 +60,7 @@ export default {
     .wrapper
         width: 100%
         height: 0
-        padding-bottom: 56.22%
+        padding-bottom: 100%
         .gallery-img
             width: 100%
         .swiper-pagination
